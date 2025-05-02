35614339168fcb8da8234cd30c9f110665c04e98
Marubi 254 <felixmarubi2005@gmail.com>
2025-05-02 13:34:03 +0300

    commiting changes

--- signUp.js
+++ signUp.js
@@ -5,10 +5,6 @@ const name=document.getElementById('name').value;
 const email=document.getElementById('email').value;
 const password=document.getElementById('password').value;
 
-if(password){
-
-
-}
 
 console.log('signing up user',{name,email,password});
 alert('signed up successfully');
